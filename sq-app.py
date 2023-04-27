286d76f45e696edfa3986c1630aaa51f61cbc0cd
ambrde <129999830+ambrde@users.noreply.github.com>
2023-04-27 14:45:50 -0700

    Update sq-app.py

--- sq-app.py
+++ sq-app.py
@@ -34,23 +34,27 @@ def renderPage3():
 def renderResults():
     session["q3ans"]=request.form['q3ans']
     check = checkAnswers()
-    return render_template("sq-results.html", c1=check[0], c2=check[1], c3=check[2])
+    return render_template("sq-results.html", c1=check[0], c2=check[1], c3=check[2], score=check[3])
     
  
 def checkAnswers():
+    score = 0
     if session['q1ans']=="vehicles next to the broken line may pass":
         c1 = "That's correct!"
+        score = score + 1
     else:
         c1 = "That's incorrect. The correct answer for question 1 was 'vehicles next to the broken line may pass.'"
     if session['q2ans']=="at all times":
         c2 = "That's correct!"
+        score = score + 1
     else:
         c2 = "That's incorrect. The correct answer for question 2 was 'at all times.'"
     if session['q3ans']=="when turning (within 200 ft of an intersection)":
         c3 = "That's correct!"
+        score = score + 1
     else:
         c3 = "That's incorrect. The correct answer for question 3 was 'when turning (within 200 ft of an intersection).'"
-    return c1, c2, c3
+    return c1, c2, c3, score
 
 if __name__=="__main__":
     app.run(debug=False)
